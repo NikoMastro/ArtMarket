28beb3a8791da65c34e0cb3dba16d38b3f861df5
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-08-13 14:11:43 +0900

    edit product show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :products, only: [:index, :new, :create] do
+  resources :products, only: [:index, :show, :new, :create] do
     resources :reviews, only: :create
     resources :renting_requests, only: :create
   end
