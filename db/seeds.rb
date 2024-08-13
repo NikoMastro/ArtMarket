849bcaccbf06904aa36fd0ab56293a9d0d541f62
Sunjun Hwang <sunjun.software@gmail.com>
2024-08-13 11:50:47 +0900

    made seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+User.create!(
+  name: "Canned Heat",
+  address: "In my heels",
+  password: "12345678",
+  email_address: "jami@roquai.com"
+)
+User.create!(
+  name: "Astley",
+  address: "Never gonna give you up",
+  password: "12345678",
+  email_address: "nevergonna@letudown.com"
+)
+
+Product.create!(
+  title: "Luxury Shoes",
+  description: "I was born to run and built to last, you've never seen my feet 'cause they're goin' so fast",
+  price: 99,
+  category: "clothes",
+  user: User.find(1)
+)
+ puts "monkey"
