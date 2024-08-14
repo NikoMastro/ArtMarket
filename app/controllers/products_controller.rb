65b413e4666b9f0da3a995ae99b89b72ed46051d
Pratchavee Amornsetthachai <pratchavee.amorn@gmail.com>
2024-08-14 14:17:45 +0900

    make the correction

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    # @product.user = current_user
+    @product.user = current_user
     if @product.save
       redirect_to product_path(@product)
     else
