1f94351db67e83c6be1330afcfe1f183b6a5369e
Pratchavee Amornsetthachai <pratchavee.amorn@gmail.com>
2024-08-14 09:09:00 +0900

    did the javascript part

--- app/javascript/controllers/reviews_controller.js
+++ app/javascript/controllers/reviews_controller.js
@@ -1,3 +1,21 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = [ 'form' ]
+
+  connect() {
+    console.log('Hello from reviews_controller.js')
+    // console.log(this.testTarget)
+  }
+
+  toggle(event) {
+    event.preventDefault()
+    this.formTarget.classList.toggle("d-none")
+  }
+}
+
+
+
 // create(event) {
 //   event.preventDefault();
 
