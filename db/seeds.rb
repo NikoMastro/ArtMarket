388872186e4788f32b858f46dcfd3e405c6cf403
Sunjun Hwang <sunjun.software@gmail.com>
2024-08-14 14:25:46 +0900

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,7 +53,46 @@ Product.create!(
   user_id: User.all.sample.id
 )
 
+Product.create!(
+  title: "Ferrari Purosangue",
+  description: "4-seater cabriolet",
+  price: rand(100000..10000000),
+  category: "car",
+  user_id: User.all.sample.id
+)
 
+Product.create!(
+  title: "Ferrari 296 GTS",
+  description: "2-seater cabriolet",
+  price: rand(100000..10000000),
+  category: "car",
+  user_id: User.all.sample.id
+)
+
+Product.create!(
+  title: "Rolex Submariner",
+  description: "
+  This is a superb example of a 'no-date' Submariner—a model that is prized for its cleaner, pared-down look and symmetrical design that many collectors appreciate. The ref 114060 follows on from and updates the 14060M, which began life with series P in the year 2000—but its predecessor, the plain 14060, was in production as early as 1990. Fans will also point out that the original Submariners, from their introduction in 1953, did not have date windows. While this model continues to use the flawless in-house calibre 3130, it vastly improves the bezel with high-tech ceramic and also upgrades the bracelet.\n\nThis Rolex Submariner 114060 has a black dial and a bracelet strap. This Rolex has undergone a thorough inspection of water resistance, accuracy, functionality and condition to determine the level of reconditioning required to meet our strict standards. It has also been referenced against technical documents and manufacturer records where available to ensure authenticity and a clean history. All our watches have a 24-month warranty for your peace of mind. One of our skilled polishing technicians has removed marks on the case and bracelet of this watch, brought about by daily wear and tear, which is why this watch does not come with the remaining manufacturer’s warranty.",
+  price: rand(100000..10000000),
+  category: "watch",
+  user_id: User.all.sample.id
+)
+
+Product.create!(
+  title: "Morganite Arden Diamond Ring with Lab Emerald Accents",
+  description: "Budding round and marquise diamonds, lab grown emeralds, and green tsavorite garnets glimmer on each side of the center gem in this nature-inspired trellis ring (1/15 total carat weight).",
+  price: rand(100000..10000000),
+  category: "jewelry",
+  user_id: User.all.sample.id
+)
+
+Product.create!(
+  title: "Freesia 2mm Solitaire Engagement Ring",
+  description: "Lustrous metal flows upwards like vines reaching towards the sun in this subtly nature-inspired solitaire ring that secures the center stone in classic claw prongs. The band gently tapers from 2mm to 1.2mm to complete the look with a sleek finish.\n\nAs a Brilliant Pick, this piece displays the best and brightest in fashion and design and is beloved by Brilliant Earth designers and customers alike.",
+  price: rand(100000..10000000),
+  category: "jewelry",
+  user_id: User.all.sample.id
+)
 # puts "generating 10 Product models..."
 # 10.times do
 #   product = Product.new(
