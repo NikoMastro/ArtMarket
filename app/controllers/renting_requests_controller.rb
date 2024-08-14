392e0bd02391639d2de30c272a356e3a03ff7634
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-08-14 12:00:21 +0900

    making requests index page

--- app/controllers/renting_requests_controller.rb
+++ app/controllers/renting_requests_controller.rb
@@ -1,6 +1,10 @@
 class RentingRequestsController < ApplicationController
   #this is our bookmark
   #see all requests as a seller (Owner)
+  def index
+    @renting_requests = RentingRequest.where(user: current_user)
+
+  end
   #see all requests as a client
   #accept/reject a request
 end
