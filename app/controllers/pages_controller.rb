e243ab5a1b8bb4e42bcd3c5f18e1ffff0acd0a85
pierre nikolas contessotto <pierrenikolas@gmail.com>
2024-08-16 04:54:59 +0900

    index card and navbar finished.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :terms, :about]
   def home
   end
 
   def about
+  end
+  def terms
   end
 
 end
